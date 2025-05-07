10a39f0e03fcb07c174ccb0e63686b59e6d789a6
leonardsiar <leonard.siar@estl.sg>
2025-05-07 07:02:27 +0000

    Added function to key in username and display on Page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var age=20
 console.log(age)
 age =60
 
-var username="leonard"
+
 var password="techup"
 
 var isMale=false
@@ -10,15 +10,30 @@ var isLoggedIn=false
 
 var blog_posts = ["Today was a good day", "The sky is blue", 
     "It's raining heavily now"]
-    console.log(username);
-    console.log(password);
-    console.log(isMale);
-    console.log(isLoggedIn);
-    console.log(blog_posts);
+    
     
     if (isMale == true) {
         console.log("You are male")
     } else{
         console.log("You are not male")
     }
-    
+    while (age < 70) {
+        console.log("Your age is " + age );
+        age = age + 1;
+    }
+    function sayHello() {
+        // Get the value from the input field
+        const username = document.getElementById("usernameInput").value;
+
+        // Check if the input is empty
+        if (username === "") {
+            alert("Please enter your name.");
+            return;
+        }
+        // Display the greeting message
+        // in the HTML element with id "greeting"
+        // Use template literals to include the username
+
+        document.getElementById("greeting").innerHTML = `<p>Hello, ${username}!</p>`
+    }
+
