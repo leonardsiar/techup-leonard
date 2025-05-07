dab92c7bd57be9bd9cba7949d2e885e12dca022c
leonardsiar <leonard.siar@estl.sg>
2025-05-07 05:59:22 +0000

    Add JavaScript functionality and button to index.html

---
+++ script.js
@@ -0,0 +1,24 @@
+var age=20
+console.log(age)
+age =60
+
+var username="leonard"
+var password="techup"
+
+var isMale=false
+var isLoggedIn=false
+
+var blog_posts = ["Today was a good day", "The sky is blue", 
+    "It's raining heavily now"]
+    console.log(username);
+    console.log(password);
+    console.log(isMale);
+    console.log(isLoggedIn);
+    console.log(blog_posts);
+    
+    if (isMale == true) {
+        console.log("You are male")
+    } else{
+        console.log("You are not male")
+    }
+    
